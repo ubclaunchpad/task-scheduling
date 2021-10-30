2982eb3b330ba57483427c57d4f62c555c5b0378
Gokce Dilek <39871099+gokcedilek@users.noreply.github.com>
2021-10-30 12:25:35 -0700

    Revert "Basic CRUD Operations for Tasks using Firestore"

--- backend/functions/src/index.ts
+++ backend/functions/src/index.ts
@@ -1,55 +1,9 @@
 import * as functions from "firebase-functions";
-import * as admin from "firebase-admin";
-import {TaskTypeNoID} from "./types";
-admin.initializeApp();
-// Start writing Firebase Functions
-// https://firebase.google.com/docs/functions/typescript
+
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
 //
 export const helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
   response.send("Hello from Firebase!");
 });
-
-export const createTask = functions.https
-    .onRequest(async (request, response) => {
-      const {title, description} = request.body;
-      const task: TaskTypeNoID = {
-        title,
-        description,
-        createdAt: new Date().toISOString(),
-      };
-      const ref = await admin.firestore().collection("tasks").add(task);
-      response.send(ref.id);
-    });
-
-export const getTask = functions.https
-    .onRequest(async (request, response) => {
-      const id = request.query.id as string;
-      const ref = await admin.firestore().collection("tasks").doc(id).get();
-      response.send(ref.data());
-    }
-    );
-
-export const updateTask = functions.https
-    .onRequest(async (request, response) => {
-      const id = request.query.id as string;
-      const {title, description} = request.body;
-      const ref = await admin.firestore().collection("tasks").doc(id);
-      await ref.update({
-        title,
-        description,
-        updatedAt: new Date().toISOString(),
-      });
-      response.send("ok");
-    }
-    );
-
-export const deleteTask = functions.https
-    .onRequest(async (request, response) => {
-      const id = request.query.id as string;
-      const ref = await admin.firestore().collection("tasks").doc(id);
-      await ref.delete();
-      response.send("ok");
-    }
-    );
-
