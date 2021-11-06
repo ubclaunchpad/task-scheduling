c85743be53f4b033a7ad7dda4bdaecce2585235a
Philip Zhang <philipzhang0@gmail.com>
2021-11-06 11:32:39 -0700

    Initialized group type

--- backend/functions/src/types.ts
+++ backend/functions/src/types.ts
@@ -1,19 +1,6 @@
-export enum Priority {
-  LOW = 'LOW',
-  MEDIUM = 'MEDIUM',
-  HIGH = 'HIGH',
+export type GroupType = {
+    creator: string; // replace with userId
+    members: string[]; // replace with userId[]
+    name: string;
+    tasks: string[];
 }
-
-export type TaskType = {
-  title: string;
-  creator: string;
-  priority?: Priority;
-  assignees?: string[];
-  groupId?: string;
-  dueDate?: string;
-  description?: string;
-  createdAt?: string;
-  tags?: string[];
-}
-
-export type TaskTypeWithID = { id: string } & TaskType;
